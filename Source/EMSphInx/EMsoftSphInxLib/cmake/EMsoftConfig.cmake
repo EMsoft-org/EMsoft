d4f03c62f1d4f4ffcba7420b6d7949cbbc2fd7a8
Marc DeGraef <degraef@cmu.edu>
2019-03-22 09:13:26 -0400

    removes all files

--- Source/EMSphInx/EMsoftSphInxLib/cmake/EMsoftConfig.cmake
+++
@@ -1,6 +0,0 @@
-include("${CMAKE_CURRENT_LIST_DIR}/EMSoftLibTargets.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/EMSoftLibHDFTargets.cmake")
-
-set(EMsoft_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/../../../include")
-set(EMsoft_LIB_DIRS "${CMAKE_CURRENT_LIST_DIR}/../../../lib;${CMAKE_CURRENT_LIST_DIR}/../../../bin")
-
