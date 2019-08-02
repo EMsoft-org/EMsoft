a57146c1aab946dc6a68d393fefcf20adc991159
Marc DeGraef <degraef@cmu.edu>
2019-08-02 15:00:26 -0400

    adds file reading loop to EMdpextract program

--- Source/DictionaryIndexing/EMdpextract.f90
+++ Source/DictionaryIndexing/EMdpextract.f90
@@ -39,6 +39,7 @@
 !> @note all we need to run this program is the file name of the dot product file
 !
 !> @date 02/05/19 MDG 1.0 original
+!> @date 08/02/19 MDG 1.1 add file reading loop
 !--------------------------------------------------------------------------
 program EMdpextract
 
@@ -62,9 +63,10 @@ character(fnlen)                        :: progname, progdesc, dpfilebase, dpfil
 type(HDFobjectStackType),pointer        :: HDF_head
 type(EBSDDIdataType)                    :: EBSDDIdata
 type(EBSDIndexingNameListType)          :: dinl
-integer(kind=irg)                       :: hdferr, shp(2), jj, nx, ny
+integer(kind=irg)                       :: hdferr, shp(2), jj, nx, ny, io_int(1), another 
 real(kind=sgl)                          :: mi, ma
-logical                                 :: fexists 
+logical                                 :: fexists
+character(fnlen)                        :: cwd
 
 ! declare variables for use in object oriented image module
 integer                                 :: iostat
@@ -79,173 +81,185 @@ progdesc = 'Extract a number of images from a dictionary indexing dot product fi
 
 ! print some information
 call EMsoft(progname, progdesc)
-
-
-! ask the user for the dot product file name without the .h5 extension
-call ReadValue('Enter the name of the dot product file without the .h5 extension:', dpfilebase)
-dpfile = trim(dpfilebase)//'.h5'
-
-call Message (' looking for file '//trim(dpfile))
-
-! read all the image-type arrays from the file
 call h5open_EMsoft(hdferr)
-call readEBSDDotProductFile(dpfile, dinl, hdferr, EBSDDIdata, &
-                            getADP=.TRUE., &
-                            getKAM=.TRUE., &
-                            getCI=.TRUE., &
-                            getIQ=.TRUE., & 
-                            getOSM=.TRUE., & 
-                            presentFolder=.TRUE.) 
+
+another = 1
+
+call getcwd(cwd)
+write(*,*) 'cwd = ',trim(cwd)
+
+do while (another.eq.1) 
+
+  ! ask the user for the dot product file name without the .h5 extension
+  dpfilebase = ''
+  call ReadValue('Enter the name of the dot product file without the .h5 extension:', dpfilebase)
+  dpfile = trim(cwd)//'/'//trim(dpfilebase)//'.h5'
+
+  call Message (' looking for file '//trim(dpfile))
+
+  ! read all the image-type arrays from the file
+
+  call readEBSDDotProductFile(dpfile, dinl, hdferr, EBSDDIdata, &
+                              getADP=.TRUE., &
+                              getKAM=.TRUE., &
+                              getCI=.TRUE., &
+                              getIQ=.TRUE., & 
+                              getOSM=.TRUE., & 
+                              presentFolder=.TRUE.) 
+
+  call Message('   found file and read data arrays')
+
+  ! take these arrays and generate image files for each of them; place them in a folder with the dpfilebase name 
+  dirname = trim(dpfilebase)
+  inquire(file=trim(dirname),exist=fexists)
+  if (.not.(fexists)) then
+    call system('mkdir '//trim(dirname))
+    write (*,*) trim(dirname),' folder has been created'
+  end if
+  call chdir(trim(dirname))
+
+  ! ==============================
+  ! ==============================
+  ! ==============================
+  ! ADP map
+  image_filename = trim(dpfilebase)//'_ADP.tiff'
+  shp = shape(EBSDDIdata%ADP)
+  nx = shp(1)
+  ny = shp(2)
+  allocate(output_image(nx,ny))
+  output_image = EBSDDIdata%ADP
+  im2 = image_t(output_image)
+  if(im2%empty()) call Message("EMEBSDDIpreview","failed to convert array to image")
+
+  ! this data set is already scaled to byte range
+
+  ! create the file
+  call im2%write(trim(image_filename), iostat, iomsg) ! format automatically detected from extension
+  if(0.ne.iostat) then
+    call Message(" failed to write image to file : "//iomsg)
+  else  
+    call Message('  ADP array written to '//trim(image_filename))
+  end if 
+  deallocate(output_image,EBSDDIdata%ADP)
+
+  ! ==============================
+  ! ==============================
+  ! ==============================
+  ! CI map
+  image_filename = trim(dpfilebase)//'_CI.tiff'
+  allocate(output_image(nx,ny))
+
+  mi = minval(EBSDDIdata%CI)
+  EBSDDIdata%CI = EBSDDIdata%CI - mi
+  ma = maxval(EBSDDIdata%CI)
+  output_image = 0
+
+  ! CI is a 1-D array; map onto 2-D array
+  do jj = 1,ny
+      output_image(1:nx,jj) = int(255.0*EBSDDIdata%CI((jj-1)*nx+1:jj*nx)/ma)
+  end do
+
+  im2 = image_t(output_image)
+  if(im2%empty()) call Message("EMEBSDDIpreview","failed to convert array to image")
+
+  ! create the file
+  call im2%write(trim(image_filename), iostat, iomsg) ! format automatically detected from extension
+  if(0.ne.iostat) then
+    call Message(" failed to write image to file : "//iomsg)
+  else  
+    call Message('  CI array written to '//trim(image_filename))
+  end if 
+  deallocate(output_image,EBSDDIdata%CI)
+
+  ! ==============================
+  ! ==============================
+  ! ==============================
+  ! IQ map
+  image_filename = trim(dpfilebase)//'_IQ.tiff'
+  allocate(output_image(nx,ny))
+
+  mi = minval(EBSDDIdata%IQ)
+  EBSDDIdata%IQ = EBSDDIdata%IQ - mi
+  ma = maxval(EBSDDIdata%IQ)
+  output_image = 0
+
+  ! IQ is a 1-D array; map onto 2-D array
+  do jj = 1,ny
+      output_image(1:nx,jj) = int(255.0*EBSDDIdata%IQ((jj-1)*nx+1:jj*nx)/ma)
+  end do
+
+  im2 = image_t(output_image)
+  if(im2%empty()) call Message("EMEBSDDIpreview","failed to convert array to image")
+
+  ! create the file
+  call im2%write(trim(image_filename), iostat, iomsg) ! format automatically detected from extension
+  if(0.ne.iostat) then
+    call Message(" failed to write image to file : "//iomsg)
+  else  
+    call Message('  IQ array written to '//trim(image_filename))
+  end if 
+  deallocate(output_image,EBSDDIdata%IQ)
+
+  ! ==============================
+  ! ==============================
+  ! ==============================
+  ! KAM map
+  image_filename = trim(dpfilebase)//'_KAM.tiff'
+  allocate(output_image(nx,ny))
+
+  mi = minval(EBSDDIdata%KAM)
+  EBSDDIdata%KAM = EBSDDIdata%KAM - mi
+  ma = maxval(EBSDDIdata%KAM)
+  output_image = 0
+
+  output_image = int(255.0*EBSDDIdata%KAM/ma)
+
+  im2 = image_t(output_image)
+  if(im2%empty()) call Message("EMEBSDDIpreview","failed to convert array to image")
+
+  ! create the file
+  call im2%write(trim(image_filename), iostat, iomsg) ! format automatically detected from extension
+  if(0.ne.iostat) then
+    call Message(" failed to write image to file : "//iomsg)
+  else  
+    call Message('  KAM array written to '//trim(image_filename))
+  end if 
+  deallocate(output_image,EBSDDIdata%KAM)
+
+  ! ==============================
+  ! ==============================
+  ! ==============================
+  ! OSM map
+  image_filename = trim(dpfilebase)//'_OSM.tiff'
+  allocate(output_image(nx,ny))
+
+  mi = minval(EBSDDIdata%OSM)
+  EBSDDIdata%OSM = EBSDDIdata%OSM - mi
+  ma = maxval(EBSDDIdata%OSM)
+  output_image = 0
+
+  output_image = int(255.0*EBSDDIdata%OSM/ma)
+
+  im2 = image_t(output_image)
+  if(im2%empty()) call Message("EMEBSDDIpreview","failed to convert array to image")
+
+  ! create the file
+  call im2%write(trim(image_filename), iostat, iomsg) ! format automatically detected from extension
+  if(0.ne.iostat) then
+    call Message(" failed to write image to file : "//iomsg)
+  else  
+    call Message('  OSM array written to '//trim(image_filename))
+  end if 
+  deallocate(output_image,EBSDDIdata%OSM)
+
+  call Message('----')
+  call ReadValue(' Another one ? (1/0) :', io_int)
+  another = io_int(1)
+
+  call chdir(trim(cwd))
+end do 
+
 call h5close_EMsoft(hdferr)
-call Message('   found file and read data arrays')
-
-! take these arrays and generate image files for each of them; place them in a folder with the dpfilebase name 
-dirname = trim(dpfilebase)
-inquire(file=trim(dirname),exist=fexists)
-if (.not.(fexists)) then
-  call system('mkdir '//trim(dirname))
-  write (*,*) trim(dirname),' folder has been created'
-end if
-call chdir(trim(dirname))
-
-! ==============================
-! ==============================
-! ==============================
-! ADP map
-image_filename = trim(dpfilebase)//'_ADP.tiff'
-shp = shape(EBSDDIdata%ADP)
-nx = shp(1)
-ny = shp(2)
-allocate(output_image(nx,ny))
-output_image = EBSDDIdata%ADP
-im2 = image_t(output_image)
-if(im2%empty()) call Message("EMEBSDDIpreview","failed to convert array to image")
-
-! this data set is already scaled to byte range
-
-! create the file
-call im2%write(trim(image_filename), iostat, iomsg) ! format automatically detected from extension
-if(0.ne.iostat) then
-  call Message(" failed to write image to file : "//iomsg)
-else  
-  call Message('  ADP array written to '//trim(image_filename))
-end if 
-deallocate(output_image)
-
-! ==============================
-! ==============================
-! ==============================
-! CI map
-image_filename = trim(dpfilebase)//'_CI.tiff'
-allocate(output_image(nx,ny))
-
-mi = minval(EBSDDIdata%CI)
-EBSDDIdata%CI = EBSDDIdata%CI - mi
-ma = maxval(EBSDDIdata%CI)
-output_image = 0
-
-! CI is a 1-D array; map onto 2-D array
-do jj = 1,ny
-    output_image(1:nx,jj) = int(255.0*EBSDDIdata%CI((jj-1)*nx+1:jj*nx)/ma)
-end do
-
-im2 = image_t(output_image)
-if(im2%empty()) call Message("EMEBSDDIpreview","failed to convert array to image")
-
-! create the file
-call im2%write(trim(image_filename), iostat, iomsg) ! format automatically detected from extension
-if(0.ne.iostat) then
-  call Message(" failed to write image to file : "//iomsg)
-else  
-  call Message('  CI array written to '//trim(image_filename))
-end if 
-deallocate(output_image)
-
-! ==============================
-! ==============================
-! ==============================
-! IQ map
-image_filename = trim(dpfilebase)//'_IQ.tiff'
-allocate(output_image(nx,ny))
-
-mi = minval(EBSDDIdata%IQ)
-EBSDDIdata%IQ = EBSDDIdata%IQ - mi
-ma = maxval(EBSDDIdata%IQ)
-output_image = 0
-
-! IQ is a 1-D array; map onto 2-D array
-do jj = 1,ny
-    output_image(1:nx,jj) = int(255.0*EBSDDIdata%IQ((jj-1)*nx+1:jj*nx)/ma)
-end do
-
-im2 = image_t(output_image)
-if(im2%empty()) call Message("EMEBSDDIpreview","failed to convert array to image")
-
-! create the file
-call im2%write(trim(image_filename), iostat, iomsg) ! format automatically detected from extension
-if(0.ne.iostat) then
-  call Message(" failed to write image to file : "//iomsg)
-else  
-  call Message('  IQ array written to '//trim(image_filename))
-end if 
-deallocate(output_image)
-
-! ==============================
-! ==============================
-! ==============================
-! KAM map
-image_filename = trim(dpfilebase)//'_KAM.tiff'
-allocate(output_image(nx,ny))
-
-mi = minval(EBSDDIdata%KAM)
-EBSDDIdata%KAM = EBSDDIdata%KAM - mi
-ma = maxval(EBSDDIdata%KAM)
-output_image = 0
-
-output_image = int(255.0*EBSDDIdata%KAM/ma)
-
-im2 = image_t(output_image)
-if(im2%empty()) call Message("EMEBSDDIpreview","failed to convert array to image")
-
-! create the file
-call im2%write(trim(image_filename), iostat, iomsg) ! format automatically detected from extension
-if(0.ne.iostat) then
-  call Message(" failed to write image to file : "//iomsg)
-else  
-  call Message('  KAM array written to '//trim(image_filename))
-end if 
-deallocate(output_image)
-
-! ==============================
-! ==============================
-! ==============================
-! OSM map
-image_filename = trim(dpfilebase)//'_OSM.tiff'
-allocate(output_image(nx,ny))
-
-mi = minval(EBSDDIdata%OSM)
-EBSDDIdata%OSM = EBSDDIdata%OSM - mi
-ma = maxval(EBSDDIdata%OSM)
-output_image = 0
-
-output_image = int(255.0*EBSDDIdata%OSM/ma)
-
-im2 = image_t(output_image)
-if(im2%empty()) call Message("EMEBSDDIpreview","failed to convert array to image")
-
-! create the file
-call im2%write(trim(image_filename), iostat, iomsg) ! format automatically detected from extension
-if(0.ne.iostat) then
-  call Message(" failed to write image to file : "//iomsg)
-else  
-  call Message('  OSM array written to '//trim(image_filename))
-end if 
-deallocate(output_image)
-
-
-
-
-
 
 
 end program EMdpextract
