4f6f97281c07ab881b0f92109d381d0a61351ef3
Michael Jackson <mike.jackson@bluequartz.net>
2019-10-14 11:34:16 -0400

    Improvements to SHTfile project inclusion.
    Will now look for SHTfile inside of EMSoft/ExternalProjects. If it is not found
    then it will clone it from github.com/emsoft-org/SHTfile
    
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- Source/EMsoftLib/sht_file.cpp
+++ Source/EMsoftLib/sht_file.cpp
@@ -1,36 +1,31 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *                                                                     *
- * Copyright (c) 2019-2019, De Graef Group, Carnegie Mellon University *
- * All rights reserved.                                                *
- *                                                                     *
- * Author: William C. Lenthe                                           *
- *                                                                     *
- * This package is free software; you can redistribute it and/or       *
- * modify it under the terms of the GNU General Public License as      *
- * published by the Free Software Foundation; either version 2 of the  *
- * License, or (at your option) any later version.                     *
- *                                                                     *
- * This program is distributed in the hope that it will be useful,     *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of      *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       *
- * GNU General Public License for more details.                        *
- *                                                                     *
- * You should have received a copy of the GNU General Public License   *
- * along with this program; if not, check the Free Software Foundation *
- * website: <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>   *
- *                                                                     *
- *                                                                     *
- * Interested in a commercial license? Contact:                        *
- *                                                                     *
- * Center for Technology Transfer and Enterprise Creation              *
- * 4615 Forbes Avenue, Suite 302                                       *
- * Pittsburgh, PA 15213                                                *
- *                                                                     *
- * phone. : 412.268.7393                                               *
- * email  : innovation@cmu.edu                                         *
- * website: https://www.cmu.edu/cttec/                                 *
- *                                                                     *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* 
+* Copyright (c) 2018-2019, Marc De Graef Research Group/Carnegie Mellon University  *
+* All rights reserved.                                                              *
+*                                                                                   *
+* Redistribution and use in source and binary forms, with or without                *
+* modification, are permitted provided that the following conditions are met:       *
+*                                                                                   *
+*     - Redistributions of source code must retain the above copyright notice, this *
+*       list of conditions and the following disclaimer.                            *
+*     - Redistributions in binary form must reproduce the above copyright notice,   *
+*       this list of conditions and the following disclaimer in the documentation   *
+*       and/or other materials provided with the distribution.                      *
+*     - Neither the copyright holder nor the names of its                           *
+*       contributors may be used to endorse or promote products derived from        *
+*       this software without specific prior written permission.                    *
+*                                                                                   *
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"       *
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE         *
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE    *
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE      *
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL        *
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR        *
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER        *
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,     *
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE         *
+* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.          *
+*                                                                                   *
+*/
 
 #include "sht_file.hpp"
 
@@ -48,10 +43,10 @@ extern "C" {
   //@param iprm: integer parameters {# electrons, electron multiplier, numsx, npx, latgridtype}
   //@param bw : bandwidth
   //@param alm: actual harmonics (uncompressed format)
-  int writeSHTfile_(char * fn, char const * nt, char const * doi,
-                  int32_t * sgN, int32_t * sgS, int32_t * nAt, int32_t * aTy, float * aCd, float * lat,
-                  float * fprm, int32_t * iprm,
-                  int32_t * bw,double * alm) {
+  int writeSHTfile_(char* fn, const char* nt, const char* doi,
+                  int32_t* sgN, int32_t* sgS, int32_t* nAt, int32_t* aTy, float* aCd, float* lat,
+                  float* fprm, int32_t* iprm,
+                  int32_t* bw,double* alm) {
 	  return sht::File::EMsoftEBSDRet(fn, nt, doi, *sgN, *sgS, *nAt, aTy, aCd, lat, fprm, iprm, *bw, alm);
   }
 }
