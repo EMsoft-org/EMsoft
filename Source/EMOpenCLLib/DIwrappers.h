cb9e172bd1cb16b975ba786617a03ac334c5c820
Marc DeGraef <degraef@cmu.edu>
2020-02-22 10:36:48 -0500

    splits wrapper routine into two: timing updates and data return

--- Source/EMOpenCLLib/DIwrappers.h
+++ Source/EMOpenCLLib/DIwrappers.h
@@ -13,12 +13,18 @@ extern "C" {
 * @param int   number of loops completed
 * @param int   total number of indexing loops
 * @param float time remaining in seconds (first call: total expected time)
+*/
+typedef void (*ProgCallBackTypeTimingdriver)(size_t, int, int, float);
+
+/**
+* @brief This is the typedef for a call back function that is used in the EMsoft library.
+* @param size_t Unique integer that designates which C++ object did the call into EMsoft
 * @param int   number of triplets in Euler dictionary array
 * @param float pointer to array to possible euler angle triplets
 * @param float pointer to the array of dot products
 * @param int   pointer to the array of indices into the orientation array
 */
-typedef void (*ProgCallBackTypeDIdriver)(size_t, int, int, float, int, float**, float**, int32_t**);
+typedef void (*ProgCallBackTypeDIdriver)(size_t, int, float**, float**, int32_t**);
 
 /**
 * @brief This is the typedef for an OpenCL error call back function that is used in the EMsoft library.
