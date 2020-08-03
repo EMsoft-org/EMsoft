d4dc33aa919ca1b71f0de530d815d1d0aded0c62
Darkhunter9 <zihaod@andrew.cmu.edu>
2020-08-03 17:51:55 -0400

    1. remove using namespace std; 2. replace old style casting with static_cast<float*> style; 3. add function doc.

--- Source/EMsoftWrapperLib/DictionaryIndexing/PyUtils.cpp
+++ Source/EMsoftWrapperLib/DictionaryIndexing/PyUtils.cpp
@@ -4,38 +4,32 @@
 #include <stdexcept>
 #include "PyUtils.hpp"
 
-using namespace std;
 
-// =====
-// LISTS
-// =====
-
-PyObject* vectorToList_Float(const vector<float> &data) {
+/* Convert float (single precision) vector in C/C++ to float (double precision) list in Python */
+PyObject* vectorToList_Float(const std::vector<float> &data) {
   PyObject* listObj = PyList_New( data.size() );
-    if (!listObj) throw logic_error("Unable to allocate memory for Python list");
+    if (!listObj) throw std::logic_error("Unable to allocate memory for Python list");
     for (unsigned int i = 0; i < data.size(); i++) {
-        PyObject *num = PyFloat_FromDouble( (double) data[i]);
+        PyObject *num = PyFloat_FromDouble( static_cast<double>(data[i]));
         if (!num) {
             Py_DECREF(listObj);
-            throw logic_error("Unable to allocate memory for Python list");
+            throw std::logic_error("Unable to allocate memory for Python list");
         }
         PyList_SET_ITEM(listObj, i, num);
     }
     return listObj;
 }
 
-// ======
-// TUPLES
-// ======
 
-PyObject* vectorToTuple_Float(const vector<float> &data) {
+/* Convert float (single precision) vector in C/C++ to float (double precision) tuple in Python */
+PyObject* vectorToTuple_Float(const std::vector<float> &data) {
     PyObject* tuple = PyTuple_New( data.size() );
-    if (!tuple) throw logic_error("Unable to allocate memory for Python tuple");
+    if (!tuple) throw std::logic_error("Unable to allocate memory for Python tuple");
     for (unsigned int i = 0; i < data.size(); i++) {
-        PyObject *num = PyFloat_FromDouble( (double) data[i]);
+        PyObject *num = PyFloat_FromDouble( static_cast<double>(data[i]));
         if (!num) {
             Py_DECREF(tuple);
-            throw logic_error("Unable to allocate memory for Python tuple");
+            throw std::logic_error("Unable to allocate memory for Python tuple");
         }
         PyTuple_SET_ITEM(tuple, i, num);
     }
@@ -43,19 +37,22 @@ PyObject* vectorToTuple_Float(const vector<float> &data) {
     return tuple;
 }
 
-PyObject* vectorVectorToTuple_Float(const vector< vector< float > > &data) {
+
+/* Convert 2D float (single precision) array (vector of vectors of float) in C/C++ to
+2D float (double precision) tuple (tuple of tuples of float) in Python */
+PyObject* vectorVectorToTuple_Float(const std::vector< std::vector< float > > &data) {
     PyObject* tuple = PyTuple_New( data.size() );
-    if (!tuple) throw logic_error("Unable to allocate memory for Python tuple");
+    if (!tuple) throw std::logic_error("Unable to allocate memory for Python tuple");
     for (unsigned int i = 0; i < data.size(); i++) {
         PyObject* subTuple = NULL;
         try {
             subTuple = vectorToTuple_Float(data[i]);
-        } catch (logic_error &e) {
+        } catch (std::logic_error &e) {
             throw e;
         }
         if (!subTuple) {
             Py_DECREF(tuple);
-            throw logic_error("Unable to allocate memory for Python tuple of tuples");
+            throw std::logic_error("Unable to allocate memory for Python tuple of tuples");
         }
         PyTuple_SET_ITEM(tuple, i, subTuple);
     }
@@ -63,9 +60,10 @@ PyObject* vectorVectorToTuple_Float(const vector< vector< float > > &data) {
     return tuple;
 }
 
-// PyObject -> Vector
-vector<float> listTupleToVector_Float(PyObject* incoming) {
-    vector<float> data;
+
+/* Convert float (double precision) list/tuple in Python to float (single precision) vector in C/C++ */
+std::vector<float> listTupleToVector_Float(PyObject* incoming) {
+    std::vector<float> data;
     if (PyTuple_Check(incoming)) {
         for(Py_ssize_t i = 0; i < PyTuple_Size(incoming); i++) {
             PyObject *value = PyTuple_GetItem(incoming, i);
@@ -78,15 +76,16 @@ vector<float> listTupleToVector_Float(PyObject* incoming) {
                 data.push_back( static_cast<float>(PyFloat_AsDouble(value)) );
             }
         } else {
-            throw logic_error("Passed PyObject pointer was not a list or tuple of float!");
+            throw std::logic_error("Passed PyObject pointer was not a list or tuple of float!");
         }
     }
     return data;
 }
 
-// PyObject -> Vector
-vector<int> listTupleToVector_Int(PyObject* incoming) {
-    vector<int> data;
+
+/* Convert int list/tuple in Python to int vector in C/C++ */
+std::vector<int> listTupleToVector_Int(PyObject* incoming) {
+    std::vector<int> data;
     if (PyTuple_Check(incoming)) {
         for(Py_ssize_t i = 0; i < PyTuple_Size(incoming); i++) {
             PyObject *value = PyTuple_GetItem(incoming, i);
@@ -99,34 +98,37 @@ vector<int> listTupleToVector_Int(PyObject* incoming) {
                 data.push_back( static_cast<int>(PyFloat_AsDouble(value)) );
             }
         } else {
-            throw logic_error("Passed PyObject pointer was not a list or tuple of int!");
+            throw std::logic_error("Passed PyObject pointer was not a list or tuple of int!");
         }
     }
     return data;
 }
 
-// PyObject -> Vector
-vector<string> listTupleToVector_Str(PyObject* incoming) {
-    vector<string> data;
+
+/* Convert str list/tuple in Python to str vector in C/C++ */
+std::vector<std::string> listTupleToVector_Str(PyObject* incoming) {
+    std::vector<std::string> data;
     if (PyTuple_Check(incoming)) {
         for(Py_ssize_t i = 0; i < PyTuple_Size(incoming); i++) {
             PyObject *value = PyTuple_GetItem(incoming, i);
-            data.push_back((string) PyUnicode_AsUTF8(value) );
+            data.push_back((std::string) PyUnicode_AsUTF8(value) );
         }
     } else {
         if (PyList_Check(incoming)) {
             for(Py_ssize_t i = 0; i < PyList_Size(incoming); i++) {
                 PyObject *value = PyList_GetItem(incoming, i);
-                data.push_back((string) PyUnicode_AsUTF8(value) );
+                data.push_back((std::string) PyUnicode_AsUTF8(value) );
             }
         } else {
-            throw logic_error("Passed PyObject pointer was not a list or tuple of string!");
+            throw std::logic_error("Passed PyObject pointer was not a list or tuple of string!");
         }
     }
     return data;
 }
 
-// Create large 2D arrays
+
+/* Create large 2D float arrays;
+    User calling this function is responsible for freeing the memory.*/
 float** alloc_2d_float(int rows, int cols){
     float *data = (float *)malloc(rows*cols*sizeof(float));
     float **array= (float **)malloc(rows*sizeof(float*));
@@ -136,7 +138,9 @@ float** alloc_2d_float(int rows, int cols){
     return array;
 }
 
-// Create large 2D arrays
+
+/* Create large 2D int32 arrays;
+    User calling this function is responsible for freeing the memory.*/
 int32_t** alloc_2d_int32_t(int rows, int cols){
     int32_t *data = (int32_t *)malloc(rows*cols*sizeof(int32_t));
     int32_t **array= (int32_t **)malloc(rows*sizeof(int32_t*));
@@ -146,7 +150,9 @@ int32_t** alloc_2d_int32_t(int rows, int cols){
     return array;
 }
 
-// Create large 2D arrays
+
+/* Create large 2D double arrays;
+    User calling this function is responsible for freeing the memory.*/
 double** alloc_2d_double(int rows, int cols){
     double *data = (double *)malloc(rows*cols*sizeof(double));
     double **array= (double **)malloc(rows*sizeof(double*));
--- Source/EMsoftWrapperLib/DictionaryIndexing/PyUtils.hpp
+++ Source/EMsoftWrapperLib/DictionaryIndexing/PyUtils.hpp
@@ -10,36 +10,43 @@
 extern "C" {
 #endif
 
-using namespace std;
+/* Convert float (single precision) vector in C/C++ to float (double precision) list in Python */
+PyObject* vectorToList_Float(const std::vector<float> &data);
 
-// =====
-// LISTS
-// =====
-PyObject* vectorToList_Float(const vector<float> &data);
 
-// ======
-// TUPLES
-// ======
-PyObject* vectorToTuple_Float(const vector<float> &data);
+/* Convert float (single precision) vector in C/C++ to float (double precision) tuple in Python */
+PyObject* vectorToTuple_Float(const std::vector<float> &data);
 
-PyObject* vectorVectorToTuple_Float(const vector< vector< float > > &data);
 
-// PyObject -> Vector
-vector<float> listTupleToVector_Float(PyObject* incoming);
+/* Convert 2D float (single precision) array (vector of vectors of float) in C/C++ to
+2D float (double precision) tuple (tuple of tuples of float) in Python */
+PyObject* vectorVectorToTuple_Float(const std::vector< std::vector< float > > &data);
 
-// PyObject -> Vector
-vector<int> listTupleToVector_Int(PyObject* incoming);
 
-// PyObject -> Vector
-vector<string> listTupleToVector_Str(PyObject* incoming);
+/* Convert float (double precision) list/tuple in Python to float (single precision) vector in C/C++ */
+std::vector<float> listTupleToVector_Float(PyObject* incoming);
 
-// Create large 2D arrays
+
+/* Convert int list/tuple in Python to int vector in C/C++ */
+std::vector<int> listTupleToVector_Int(PyObject* incoming);
+
+
+/* Convert str list/tuple in Python to str vector in C/C++ */
+std::vector<std::string> listTupleToVector_Str(PyObject* incoming);
+
+
+/* Create large 2D float arrays;
+    User calling this function is responsible for freeing the memory.*/
 float** alloc_2d_float(int rows, int cols);
 
-// Create large 2D arrays
+
+/* Create large 2D int32 arrays;
+    User calling this function is responsible for freeing the memory.*/
 int32_t** alloc_2d_int32_t(int rows, int cols);
 
-// Create large 2D arrays
+
+/* Create large 2D double arrays;
+    User calling this function is responsible for freeing the memory.*/
 double** alloc_2d_double(int rows, int cols);
 
 #ifdef __cplusplus
