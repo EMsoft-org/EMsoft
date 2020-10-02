d558d6b3ed1658cb726e4df3819804a56a06dd49
Zihao Ding <zihaod@andrew.cmu.edu>
2020-10-02 15:02:54 -0400

    PyEMEBSDDI core wrapper (#118)
    * Add PyEMEBSDDI module (preview commit)
    
    * add info of PyEMEBSDDI_wrapper
    
    * delete unnecessary temp file
    
    * Rename Readme.md to README.md
    
    * 1. remove using namespace std; 2. replace old style casting with static_cast<float*> style; 3. add function doc.
    
    * replace include_directories with target_include_directories
    
    * add python requirement for EMsoftWrapperLib (as PyUtils is included in the source)
    
    * remove using namespace std
    
    * add descriptions for callback functions
    
    * 1. PyEMEBSDDI is now completely peeled from DictionaryIndexing_Wrapper;
    2. Outdated Python3 Lib search is replaced with find_package(Python3).
    
    * add an independent toggle EMsoft_ENABLE_PyEMEBSDDI to control the compilation of PyEMEBSDDI
    
    * remove spaces between two "<"
    
    * add DOxygen style comments to functions
    
    * add DOxygen style comments to functions

---
+++ Source/EMsoftWrapperLib/DictionaryIndexing/EMsoftDIwrappersCallbacks.c
@@ -0,0 +1,16 @@
+#include "EMsoftDIwrappersCallbacks.h"
+
+void DIProcessTiming(size_t instance, int loopCompleted, int totalLoops, float timeRemaining)
+{
+    return;
+}
+
+void DIProcessError(size_t instance, int nDict)
+{
+    return;
+}
+
+void DIProcessRefine(size_t instance, int param_a, int param_b)
+{
+    return;
+}
