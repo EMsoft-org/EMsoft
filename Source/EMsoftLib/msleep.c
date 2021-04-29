e2921673c78b296b11146b464dcb60fd1caa8e51
Michael Jackson <mike.jackson@bluequartz.net>
2021-04-29 17:31:18 -0400

    Use proper function calls to simply sleep the current thread for a millisecond time.
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- Source/EMsoftLib/msleep.c
+++ Source/EMsoftLib/msleep.c
@@ -1,14 +1,35 @@
 #if defined (_MSC_VER)
-#include <winsock2.h>
+#include <windows.h>
 #else
 #include <sys/time.h>
+#include <unistd.h>
 #endif
-#include <stddef.h>
 
-void msleep(int tms)
-{
-    struct timeval tv;
-    tv.tv_sec  = tms / 1000;
-    tv.tv_usec = (tms % 1000) * 1000;
-    select (0, NULL, NULL, NULL, &tv);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/**
+* @brief Sleep the current thread for specific number of milliseconds
+* @param millis The number of milliseconds to sleep the thread.
+*/
+void msleep(int millis);
+
+#ifdef __cplusplus
 }
+#endif
+
+#if defined (_MSC_VER)
+void msleep(int millis)
+{
+  Sleep(millis);
+}
+
+#else
+
+void msleep(int millis)
+{
+  // This call actually takes a microsecond argument so make the adjustment
+  usleep(millis * 1000);
+}
+#endif
